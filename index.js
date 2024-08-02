8b466db7115f3db9f94eb59903144182792aadd7
HuseinHQ <mhusein463@gmail.com>
2024-08-02 15:22:20 +0700

    fix cors

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const fs = require('fs');
 const path = require('path');
-const app = express({
-  origin: 'https://uways.huseinhk.com', // Replace with your client domain
-  methods: 'GET,POST,PUT,DELETE,PATCH',
-  allowedHeaders: 'Content-Type, Authorization, access_token, refresh_token',
-});
-const PORT = process.env.NODE_LOCAL_PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Ensure the uploads directory exists
 const uploadDir = path.join(__dirname, 'uploads', 'articles');
@@ -19,7 +15,13 @@ if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://uways.huseinhk.com', // Replace with your client domain
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    allowedHeaders: 'Content-Type, Authorization, access_token, refresh_token',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
